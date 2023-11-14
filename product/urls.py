a295530b152376de16e24571585179d12450f298
Hesam Gh <110620352+Hesamghadami@users.noreply.github.com>
2023-11-14 22:06:17 +0330

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 app_name = 'portfolio'
 
-# urlpatterns = [
-#     path("",portfolio , name='portfolio'),
-#     path("portfolio_details",portfolio_details, name='portfolio_details'),
-#     path("category/<str:cat>",portfolio,name="portfolio_cat"),
-#     path("team_member/<str:team>",portfolio,name="portfolio_team"),
-#     path("portfolio_details/<int:id>",portfolio_details,name="portfolio_details"),
+urlpatterns = [
+    # path("",portfolio , name='portfolio'),
+    # path("portfolio_details",portfolio_details, name='portfolio_details'),
+    # path("category/<str:cat>",portfolio,name="portfolio_cat"),
+    # path("team_member/<str:team>",portfolio,name="portfolio_team"),
+    # path("portfolio_details/<int:id>",portfolio_details,name="portfolio_details"),
 
-# ]
+]
 
