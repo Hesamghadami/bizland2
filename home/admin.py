ed8aec65e0399b56292a5919bbf5b95b1beccca9
fatemehd97 <bahare.khazan98@gmail.com>
2023-11-14 21:03:13 -0800

    home

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+admin.site.register(Service)
+admin.site.register(Skills)
+admin.site.register(Category)
+admin.site.register(Teammember)
+admin.site.register(Price)
+admin.site.register(Portfolio)
