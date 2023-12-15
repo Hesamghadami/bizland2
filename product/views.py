3b35173175e1e81482120ee2f83081e61baa42cf
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2023-12-15 10:07:03 -0800

    Update views.py

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .cart import Cart
 from django.views.generic import CreateView, TemplateView
 
+
     
 class PaymentView(TemplateView):
     template_name = 'product/cart.html'
